8c553316bebdc88541ce812f9518781a740e83c0
Indhu <113664856+indhuramesh@users.noreply.github.com>
2024-01-31 09:29:10 +0530

    Add files via upload

---
+++ Day4/Success.jsx
@@ -0,0 +1,15 @@
+import "../assets/Success.css"
+const BookingSuccess = () => {
+  return (
+    <div className="booking-success">
+      <h1 className="booked">Thank you for your bookings</h1>
+      <img
+        src="https://i.pinimg.com/originals/74/2f/7e/742f7ea29cbfd7fd3f4848f17e621070.gif"
+        alt="Booked Successfully Animation"
+      />
+      <h1 className="booked">Booked Successfully!</h1>
+    </div>
+  );
+};
+
+export default BookingSuccess;
